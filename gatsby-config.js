756c6fb2948f0fe96f18b31ede7506f91af4bd63
Andys Macbook Pro <andy@just-andy.uk>
2019-10-04 23:39:51 +0100

    Added MDX functionlity
    Added the plugins required to create mdx files and write them out as case studies

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,16 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+
+    `gatsby-plugin-mdx`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "case_studies",
+        path: `${__dirname}/content/case_studies/`,
+      },
+    },
+
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
