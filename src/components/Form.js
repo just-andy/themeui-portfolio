7046c7c3c6832b4a9cfab7d7bd73dad2b44fbde3
Andy Cetnarskyj WM <andrew.cetnarskyj@woodmac.com>
2019-10-08 10:08:17 +0100

    Updated profile text and added a honeypot to the netlify form

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,7 +12,10 @@ const Form = () => {
           action="/success"
           method="POST"
           data-netlify="true"
+          netlify-honeypot="bot-field"
         >
+          <input name="bot-field" />
+
           <Label htmlFor="name">Your Name: </Label>
           <Input id="name" name="name" placeholder="John Doe" />
 
