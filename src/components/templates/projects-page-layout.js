94c5a16b1eac9465a4ae0ab2239e8cb0660240ee
Andys Macbook Pro <andy@just-andy.uk>
2019-10-30 21:44:14 +0000

    added layout -broken

--- src/components/templates/projects-page-layout.js
+++ src/components/templates/projects-page-layout.js
@@ -1,18 +1,18 @@
 /** @jsx jsx */
 import { jsx,Container} from "theme-ui"
-
+import Layout from "../Layout"
 import { graphql, Link } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
 export default function PageTemplate({ data: { mdx } }) {
  
- 
+ <Layout>
         <Container>
         <h1>{mdx.frontmatter.title}</h1>
         <MDXRenderer>{mdx.body}</MDXRenderer>
         <Link to="/">Back to home</Link>
         </Container>
-
+</Layout>
   
 }
 
