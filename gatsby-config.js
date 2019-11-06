5b108808446cf2ec6e7948e7013580083bde47c2
Andys Macbook Pro <andy@just-andy.uk>
2019-11-06 22:50:24 +0000

    Fixed MDX components

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `projects`,
+        name: `experience`,
         path: `src/experience`,
       },
     },
---
+++ src/components/elements/Test.js
@@ -0,0 +1,19 @@
+/** @jsx jsx */
+import { jsx, Flex } from "theme-ui"
+
+const Test = () => {
+    return (
+        <Flex sx={{justifyContent:"space-around", backgroundColor:"colors.background", my: [2,3]}}>
+            <Flex sx={{alignItem: "start", width:["100%","180px"]}}>
+                <img src="https://via.placeholder.com/180x180.png" />
+            </Flex>
+            <Flex sx={{alignItem: "end", width:["100%","auto"]}}>
+                <h3>Header</h3>
+                <p>But if I'm not Ada,' she said, `for her hair goes in such long ringlets, and mine doesn't go in ringlets at all; and I'm sure I can't be Mabel, for I know all sorts of things, and she, oh! `Are you--are you fond--of--of dogs?' The Mouse did not notice this question, but hurriedly went on, `"--found it advisable to go with Edgar Atheling to meet William and offer him the crown. "William the Conqueror, whose cause was favoured by the pope, was soon submitted to by the pope, was soon submitted to by the English, who wanted leaders, and had been running half an hour or so, and were quite dry again.
+</p>
+            </Flex>
+        </Flex>
+    )
+}
+
+export default Test
